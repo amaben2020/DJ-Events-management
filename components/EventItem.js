a125d9c19451eefec885850fc822b03deeca8d5a
amaben2020 <amaben202phantom@gmail.com>
2021-07-31 15:35:37 +0100

    Still on the EventsItem comp

--- components/EventItem.js
+++ components/EventItem.js
@@ -5,7 +5,13 @@ import styles from '@/styles/EventItem.module.css';
 const EventItem = ({ evt }) => {
 	return (
 		<div className={styles.event}>
-			<div>{styles.img}</div>
+			<div className={styles.img}>
+				<Image
+					src={evt.image ? evt.image : '/images/event-default.png'}
+					width={170}
+					height={100}
+				/>
+			</div>
 		</div>
 	);
 };
