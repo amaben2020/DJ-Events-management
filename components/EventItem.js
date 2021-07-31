e931a3e7535f2bcc59964785b15618bb279d17c9
amaben2020 <amaben202phantom@gmail.com>
2021-07-31 15:24:30 +0100

    Building the data fetchng implementation

---
+++ components/EventItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/dist/client/image';
+const EventItem = () => {
+	return <div></div>;
+};
+
+export default EventItem;
